15014335ccc0e951f2a2eef0e9907516ab4a4904
02Alexis <m_chael1012@hotmail.com>
2025-02-23 12:33:16 -0500

    estructura de carpetas, componentes y paginas, react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { Button } from "@material-tailwind/react"
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import NoPage from "./pages/noPage/NoPage";
+import HomePage from "./pages/home/HomePage";
 
 function App() {
   return (
-    <Button>click</Button>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/*" element={<NoPage />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
