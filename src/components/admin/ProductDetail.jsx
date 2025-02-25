9ef596f3cbb782781626937afc7c18946d67b333
02Alexis <m_chael1012@hotmail.com>
2025-02-25 13:18:04 -0500

    obtener los productos y mostrarlos en el dashboard del usuario admin

--- src/components/admin/ProductDetail.jsx
+++ src/components/admin/ProductDetail.jsx
@@ -1,6 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import myContext from "../../context/myContext";
+import Loader from "../loader/Loader";
 
 const ProductDetail = () => {
+  const context = useContext(myContext);
+  const { loading, getAllProduct } = context;
+
   return (
     <div>
       <div className="py-5 flex justify-between items-center">
@@ -14,8 +20,13 @@ const ProductDetail = () => {
         </Link>
       </div>
 
+      {/* Loading  */}
+      <div className="flex justify-center relative top-20">
+        {loading && <Loader />}
+      </div>
+
       {/* table  */}
-      <div className="w-full overflow-x-auto">
+      <div className="w-full overflow-x-auto mb-5">
         <table className="w-full text-left border border-collapse sm:border-separate border-green-100 text-green-400">
           <tbody>
             <tr>
@@ -27,37 +38,77 @@ const ProductDetail = () => {
               </th>
               <th
                 scope="col"
-                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-green-100 text-slate-700 bg-slate-100"
+                className="h-12 px-6 text-md border-l first:border-l-0 border-green-100 text-slate-700 bg-slate-100 font-bold fontPara"
               >
-                Location Name
+                Imagen
               </th>
               <th
                 scope="col"
                 className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-green-100 text-slate-700 bg-slate-100"
               >
-                Action
+                Titulo
               </th>
               <th
                 scope="col"
                 className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-green-100 text-slate-700 bg-slate-100"
               >
-                Action
+                Precio
+              </th>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-green-100 text-slate-700 bg-slate-100"
+              >
+                Categoria
+              </th>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-green-100 text-slate-700 bg-slate-100"
+              >
+                Fecha
+              </th>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-green-100 text-slate-700 bg-slate-100"
+              >
+                Acciones
+              </th>
+              <th
+                scope="col"
+                className="h-12 px-6 text-md font-bold fontPara border-l first:border-l-0 border-green-100 text-slate-700 bg-slate-100"
+              >
+                Acciones
               </th>
             </tr>
-            <tr className="text-green-300">
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 ">
-                1.
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
-                {"name"}
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500  cursor-pointer ">
-                Edit
-              </td>
-              <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 ursor-pointer ">
-                Delete
-              </td>
-            </tr>
+            {getAllProduct.map((item, index) => {
+              const { id, title, price, category, date, productImageUrl } =
+                item;
+              return (
+                <tr key={index} className="text-green-300">
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 ">
+                    {index + 1}.
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    <div className="flex justify-center">
+                      <img className="w-20 " src={productImageUrl} alt="" />
+                    </div>
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    {title}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    ${price}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    {category}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 first-letter:uppercase ">
+                    {date}
+                  </td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 text-green-500 cursor-pointer "></td>
+                  <td className="h-12 px-6 text-md transition duration-300 border-t border-l first:border-l-0 border-green-100 stroke-slate-500 text-slate-500 text-red-500 cursor-pointer "></td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
