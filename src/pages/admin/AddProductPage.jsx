f5146f3bb91aafa929bb3ebddef958ed2ff727a5
02Alexis <m_chael1012@hotmail.com>
2025-02-25 12:10:07 -0500

    formulario y función para agregar nuevos productos

--- src/pages/admin/AddProductPage.jsx
+++ src/pages/admin/AddProductPage.jsx
@@ -1,7 +1,208 @@
-function AddProductPage() {
-  return (
-    <div>AddProductPage</div>
-  )
-}
+import { Timestamp, addDoc, collection } from "firebase/firestore";
+import { useContext, useState } from "react";
+import myContext from "../../context/myContext";
+import toast from "react-hot-toast";
+import { fireDB } from "../../firebase/FirebaseConfig";
+import { useNavigate } from "react-router";
+import Loader from "../../components/loader/Loader";
 
-export default AddProductPage
+const categoryList = [
+  {
+    name: "fashion",
+  },
+  {
+    name: "shirt",
+  },
+  {
+    name: "jacket",
+  },
+  {
+    name: "mobile",
+  },
+  {
+    name: "laptop",
+  },
+  {
+    name: "shoes",
+  },
+  {
+    name: "home",
+  },
+  {
+    name: "books",
+  },
+];
+
+const AddProductPage = () => {
+  const context = useContext(myContext);
+  const { loading, setLoading } = context;
+
+  // navigate
+  const navigate = useNavigate();
+
+  // product state
+  const [product, setProduct] = useState({
+    title: "",
+    price: "",
+    productImageUrl: "",
+    category: "",
+    description: "",
+    quantity: 1,
+    time: Timestamp.now(),
+    date: new Date().toLocaleString("es-co", {
+      month: "short",
+      day: "2-digit",
+      year: "numeric",
+    }),
+  });
+
+  // Add Product Function
+  const addProductFunction = async () => {
+    if (
+      product.title == "" ||
+      product.price == "" ||
+      product.productImageUrl == "" ||
+      product.category == "" ||
+      product.description == ""
+    ) {
+      return toast.error("Se requieren todos los campos");
+    }
+
+    setLoading(true);
+    try {
+      const productRef = collection(fireDB, "products");
+      await addDoc(productRef, product);
+      toast.success("Producto agregado con éxito");
+      navigate("/admin-dashboard");
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      toast.error("Producto agregado fallido");
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex justify-center items-center h-screen">
+      {loading && <Loader />}
+        {/* Login Form  */}
+        <div className="login_Form bg-green-50 px-8 py-6 border border-green-100 rounded-xl shadow-md">
+          {/* Top Heading  */}
+          <div className="mb-5">
+            <h2 className="text-center text-2xl font-bold text-green-500 ">
+              Agregar producto
+            </h2>
+          </div>
+
+          {/* Input One  */}
+          <div className="mb-3">
+            <input
+              type="text"
+              name="title"
+              placeholder="Título de producto"
+              value={product.title}
+              onChange={(e) => {
+                setProduct({
+                  ...product,
+                  title: e.target.value,
+                });
+              }}
+              className="bg-green-50 text-green-300 border border-green-200 px-2 py-2 w-96 rounded-md outline-none placeholder-green-300"
+            />
+          </div>
+
+          {/* Input Two  */}
+          <div className="mb-3">
+            <input
+              type="number"
+              placeholder="Precio del producto"
+              value={product.price}
+              onChange={(e) => {
+                setProduct({
+                  ...product,
+                  price: e.target.value,
+                });
+              }}
+              className="bg-green-50 text-green-300 border border-green-200 px-2 py-2 w-96 rounded-md outline-none placeholder-green-300"
+            />
+          </div>
+
+          {/* Input Three  */}
+          <div className="mb-3">
+            <input
+              type="text"
+              placeholder="URL de imagen del producto"
+              value={product.productImageUrl}
+              onChange={(e) => {
+                setProduct({
+                  ...product,
+                  productImageUrl: e.target.value,
+                });
+              }}
+              className="bg-green-50 text-green-300 border border-green-200 px-2 py-2 w-96 rounded-md outline-none placeholder-green-300"
+            />
+          </div>
+
+          {/* Input Four  */}
+          <div className="mb-3">
+            <select
+              value={product.category}
+              onChange={(e) => {
+                setProduct({
+                  ...product,
+                  category: e.target.value,
+                });
+              }}
+              className="w-full px-1 py-2 text-green-300 bg-green-50 border border-green-200 rounded-md outline-none  "
+            >
+              <option disabled>Select Product Category</option>
+              {categoryList.map((value, index) => {
+                const { name } = value;
+                return (
+                  <option
+                    className=" first-letter:uppercase"
+                    key={index}
+                    value={name}
+                  >
+                    {name}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+
+          {/* Input Five  */}
+          <div className="mb-3">
+            <textarea
+              name="description"
+              placeholder="Descripción del Producto"
+              value={product.description}
+              onChange={(e) => {
+                setProduct({
+                  ...product,
+                  description: e.target.value,
+                });
+              }}
+              rows="5"
+              className=" w-full px-2 py-1 text-green-300 bg-green-50 border border-green-200 rounded-md outline-none placeholder-green-300 "
+            ></textarea>
+          </div>
+
+          {/* Add Product Button  */}
+          <div className="mb-3">
+            <button
+              onClick={addProductFunction}
+              type="button"
+              className="bg-green-500 hover:bg-green-600 w-full text-white text-center py-2 font-bold rounded-md "
+            >
+              Agregar producto
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddProductPage;
